9669e07c22c72795e268b85271f75514994a91cf
Felipe Bandeira <felipeband18@gmail.com>
2012-11-15 18:13:01 -0300

    correção nos comentários

--- rlc_serie_natural.m
+++ rlc_serie_natural.m
@@ -48,9 +48,9 @@ if alfa > w0
     s2 = -alfa-sqrt(alfa^2-w0^2);
     
     f1 = (A1*exp(s1*t)+A2*exp(s2*t))-i0;
+    % encontra a derivada da função principal e substitui t por 0
     f2 = subs(diff(f1, t)-i1, t, 0);
     f1 = subs(f1, t, 0);
-
     sol = solve(f1, f2, A1, A2);
     f = simplify((sol.A1*exp(s1*t)+sol.A2*exp(s2*t)));
     
@@ -62,6 +62,7 @@ if alfa > w0
         disp('constantes:');
         disp(sol.A1);
         disp(sol.A2); 
+        
         disp('função final:');
         pretty(f);         
     end    
@@ -73,8 +74,7 @@ elseif alfa == w0
     
     f1 = (A2+A1*t)*exp(-alfa*t)-i0;
     f2 = subs(diff(f1, t)-i1, t, 0);
-    f1 = subs(f1, t, 0);
-    
+    f1 = subs(f1, t, 0); 
     sol = solve(f1, f2, A1, A2);
     f = simplify((sol.A2+sol.A1*t)*exp(-alfa*t));
     
@@ -101,7 +101,6 @@ else
     f1 = (exp(-alfa*t)*(A1*cos(wd*t)+A2*sin(wd*t)))-i0;
     f2 = subs(diff(f1, t)-i1, t, 0);
     f1 = subs(f1, t, 0);
-    
     sol = solve(f1, f2, A1, A2);
     f = simplify(exp(-alfa*t)*(sol.A1*cos(wd*t)+sol.A2*sin(wd*t)));
     
@@ -109,9 +108,11 @@ else
         disp('equações:');
         disp(f1);
         disp(f2);
+        
         disp('constantes:');
         disp(sol.A1);
-        disp(sol.A2); 
+        disp(sol.A2);
+        
         disp('função final:');
         pretty(f);
             
