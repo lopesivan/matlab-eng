14b529bb8853707cd7587e207e777a40e7243fd6
Felipe <felipeband18@gmail.com>
2012-12-07 16:50:44 -0300

    série de fourier clássica

---
+++ fourier/fourier_serie.m
@@ -0,0 +1,82 @@
+% Série de Fourier clássica
+% 
+% Felipe Bandeira, 07/12/2012. Fortaleza-CE
+%
+% Entrada:
+% T = período
+% ft = valor entre t(t0) e t(t0+1)
+% ts = tempo inicial e final do evento ft
+% N = número de interação do somatório
+% 
+% Saida:
+% a0 = componente DC
+% an = enésima constante de Fourier
+% bn = enésima constante de Fourier
+% sf = função de Fourier(simbolica)
+% f = função de Fourier
+%
+% Exemplo:
+% fourier_serie_2c(T, [-1 1], [0 1 2 ], 10)
+%                   
+%   ^ f(t)
+%   |
+%   |    ------    ------
+%   |    |    |    |    |
+%------------------------------>
+%   |    |    |    |           t
+%   ------    ------
+%   |t0   t1   t2
+%
+%   |--- T ---|
+
+function subterfugio = fourier_serie(T, ft, ts, N)
+
+% comentários existem?
+if nargin ~= 4
+    disp('erro: nao ha comentarios suficientes');
+    disp('aviso: utilizando valores padroes');
+    T = 2;
+    ft = [2 1];
+    ts = [0 1 2];
+    N = 10;
+end
+
+if length(ft)+1 ~= length(ts)
+    disp('erro: quantidade de ft ou ts não são coerentes');
+    subterfugio = NaN;
+    return;
+end
+
+syms t n;
+
+% frequência fundamental
+w0 = (2*pi)/T;
+
+a0 = 0;
+an = 0;
+bn = 0;
+
+% construindo as componentes
+for a = 1:(length(ts)-2)
+   a0 = a0 + (int(ft(a), t, ts(a), ts(a+1)) + int(ft(a+1), t, ts(a+1), ts(a+2)));
+   an = an + (int(ft(a)*cos(n*w0*t), t, ts(a), ts(a+1)) + int(ft(a+1)*cos(n*w0*t), t, ts(a+1), ts(a+2)));
+   bn = bn + (int(ft(a)*sin(n*w0*t), t, ts(a), ts(a+1)) + int(ft(a+1)*sin(n*w0*t), t, ts(a+1), ts(a+2)));
+end
+
+a0 = (1/T)*a0;
+an = (2/T)*an;
+bn = (2/T)*bn;
+
+% série de Fourier
+sf = a0 + symsum(an*cos(n*w0*t)+bn*sin(n*w0*t), n, 1, N);
+
+% cria uma função para  manipulação
+f = @(a) subs(sf, t, a);
+
+ezplot(f, [0, 10*T]);
+
+subterfugio.a0 = a0;
+subterfugio.an = an;
+subterfugio.bn = bn;
+subterfugio.sf = sf;
+subterfugio.f = f;
---
+++ fourier/fourier_serie_2c.m
@@ -0,0 +1,101 @@
+% Série de Fourier para 2 elementos
+% Felipe Bandeira, 07/12/2012. Fortaleza-CE.
+%
+% Entrada:
+% T = período
+% ft = valor entre t(t0) e t(t0+1)
+% ts = tempo inicial e final do evento ft
+% N = número de interação do somatório
+% 
+% Saida:
+% a0 = componente DC
+% an = enésima constante de Fourier
+% bn = enésima constante de Fourier
+% sf = função de Fourier(simbolica)
+% f = função de Fourier
+%
+% Exemplo:
+% fourier_serie_2c(T, [2 1], [0 1 2 ], 10)
+%
+%--------------------------------------------------------------------------
+% Subterfúgio:
+% s.m. Evasiva alegação e pretexto usado por quem 
+% procura, de maneira ardilosa, esquivar-se de 
+% dificuldades: algumas pessoas utilizam a mentira como subterfúgio 
+% para conseguir privilégios. 
+% Estratégia ou tentativa de se obter alguma coisa de modo ardil. 
+%--------------------------------------------------------------------------
+
+function subterfugio = fourier_serie_2c(T, ft, ts, N)
+
+if length(ft) > 2
+    disp('erro: só em permitido 2 elementos para ft');
+    subterfugio = NaN;
+    return;
+end
+
+if length(ts) > 3
+    disp('erro: só em permitido 3 elementos para ts');
+    subterfugio = NaN;
+    return;
+end
+
+% frequência fundamental
+w0 = (2*pi)/T;
+
+syms t n;
+
+%--------------------------------------------------------------------------
+
+% cálculo da componente DC, coeficiente a0 de Fourier
+a0 = (1/T)*(int(ft(1), t, ts(1), ts(2)) + int(ft(2), t, ts(2),ts(3)));
+
+% cálculo para a enésima 'an' componente
+an = (2/T)*(int(ft(1)*cos(n*w0*t), t, ts(1), ts(2)) + int(ft(2)*cos(n*w0*t), t, ts(2),ts(3)));
+
+% cálculo para a enésima 'bn' componente
+bn = (2/T)*(int(ft(1)*sin(n*w0*t), t, ts(1), ts(2)) + int(ft(2)*sin(n*w0*t), t, ts(2),ts(3)));
+
+% série de Fourier
+sf = a0 + symsum(an*cos(n*w0*t)+bn*sin(n*w0*t), n, 1, N);
+
+% cria uma função para  manipulação
+f = @(a) subs(sf, t, a);
+
+% harmônicas
+An = sqrt((an^2)+(bn^2));
+
+fAn = @(a) subs(An, n, a);
+
+On = atan(bn/an);
+
+fOn = @(a) subs(On, n, a);
+
+%--------------------------------------------------------------------------
+% Graficos
+
+subplot(3,1,1);
+%figure(1);
+ezplot(f, [0, 10*T]);
+
+subplot(3,1,2);
+%figure(2);
+ezplot(fAn, [0, 100*T]);
+
+subplot(3, 1, 3);
+ezplot(fOn, [0, 10*T]);
+
+%--------------------------------------------------------------------------
+
+% Os subterfúgios
+subterfugio.a0 = a0;
+subterfugio.an = an;
+subterfugio.bn = bn;
+subterfugio.sf = sf;
+subterfugio.f = f;
+subterfugio.An = An;
+subterfugio.fAn = fAn;
+subterfugio.On = On;
+subterfugio.fOn = fOn;
+
+%--------------------------------------------------------------------------
