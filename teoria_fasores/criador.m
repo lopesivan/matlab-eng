5199256d86e9de2a70442a38ae4d078d43798a68
Felipe <felipeband18@gmail.com>
2013-01-04 18:04:27 -0300

    agora os números são criados em sequencia

--- teoria_fasores/criador.m
+++ teoria_fasores/criador.m
@@ -1,6 +1,17 @@
 %% CRIADOR de números complexos
-function z = criador
-    disp('criador, 01/2013. v1');
+% dependências:
+% - catstruct.m
+
+function z = criador()
+
+    global modo;
+    
+    disp('------------------------------------------------------------');    
+
+    disp('criador, jan/2013');
+    
+    lista_numeros = cell(0);           
+    contador_numeros = 0;
 
     % LOOP PRINCIPAL
     while 1
@@ -13,32 +24,29 @@ function z = criador
         % ângulo em graus por padrão
         modo.graus = 1;
 
-        % finaliza o criador
+        %% finaliza o criador
         if strcmp(arg, 's') || strcmp(arg, 'sair')
-
-            % livra o workspace de bobagens...
-            %clearvars modo arg p_angulo p_raio;
             
-            lista = who('*_cz');
-            
-            %a = struct([]);
             z = struct([]);
             
             % começa a criação da struct z com todos os números criados
-            for c = 1:length(lista)
-                % ler o valor do número
-                eval(['valor=' char(lista(c))]);
+            for c = 1:contador_numeros
                 
-                a = struct(char(lista(c)), valor);
+                % ler o valor do número
+                eval(['valor= ' char(lista_numeros(c)) ';']);
+                
+                a = struct(char(lista_numeros(c)), valor);
+                
                 % une as estrututas
                 z = catstruct(z, a);
+                
             end
 
             disp('aviso: criador finalizado');
 
             return;
 
-        % ajuda
+        %% ajuda
         elseif strcmp(arg, 'a') || strcmp(arg, 'ajuda')
 
             disp('ajuda para o, criador');
@@ -54,85 +62,143 @@ function z = criador
             disp('d = deleta todos os números existentes');
             disp('l = lista todos os números existentes');
 
-        % cria um novo número complexo
+        %% cria um novo número complexo
         elseif strcmp(arg, 'z') || strcmp(arg, 'cria')
 
-            nome = input('nome(+_cz): ', 's');
+            disp(' ');
+            
+            while 1
+            
+                nome = input('nome: ', 's');
 
-            % padrão de criação
-            nome = strcat(nome, '_cz');
+                if strcmp(nome, '-s')
+                   break; 
+                end
 
-            %eval(['global ', nome]);
+                contador_numeros = 1 + contador_numeros;
 
-            if modo.polar == 0
+                lista_numeros(contador_numeros) = cellstr(nome);
 
-                p_real = input('parte real: ');
-                p_ima = input('parte imaginaria: ');
+                %eval(['global ', nome]);
 
-                % cria uma variável no workspace atual
-                eval([nome, '=p_real + i * p_ima']);
+                if modo.polar == 0
 
-            elseif modo.polar == 1
+                    p_real = input('parte real: ');
+                    p_ima = input('parte imaginaria: ');
 
-                p_raio = input('raio: ');
+                    % cria uma variável no workspace atual
+                    eval([nome, '=p_real + i * p_ima;']);
 
-                if modo.graus == 1
+                elseif modo.polar == 1
 
-                    p_angulo= input('ângulo(graus): ');
-                    eval([nome, '=z_polar_graus_cart(p_raio, p_angulo)']);
+                    p_raio = input('raio: ');
 
-                else
+                    if modo.graus == 1
 
-                    p_angulo = input('ângulo(radianos): ');
-                    eval([nome, '=z_polar_cart(p_raio, p_angulo)']); 
+                        p_angulo= input('ângulo(graus): ');
+                        eval([nome, '=z_polar_graus_cart(p_raio, p_angulo);']);
+
+                    else
+
+                        p_angulo = input('ângulo(radianos): ');
+                        eval([nome, '=z_polar_cart(p_raio, p_angulo);']); 
+
+                    end
 
                 end
 
+                eval(['valor =' nome ';']);
+                mostra_numero(valor);
+                disp(' ');
+                disp('------------------------------------------------------------');   
+            
             end
-            
-            %eval(['nome']);
-            
-            %eval(['a =' nome]);
-            %eval(['z = struct(nome,a)']);
 
-        % configura o modo de operação
+        %% configura o modo de operação
         elseif strcmp(arg, 'c') || strcmp(arg, 'modo')
 
             modo.polar = input('sistema polar [1/0]?');
             modo.graus = input('radiano[0] ou graus[1] ?');
 
-        % estado do modo de operação
+        %% estado do modo de operação
         elseif strcmp(arg, 'e') || strcmp(arg, 'estado')
+            
+            fprintf('\npolar = %u', modo.polar);
+            fprintf('\ngraus = %u', modo.graus);
 
-            disp('polar:');
-            disp(modo.polar);
-
-            disp('graus:');
-            disp(modo.graus);
-
-        % deleta todos os números existentes(padrão "_cz")
+        %% deleta todos os números existentes(padrão "_cz")
         elseif strcmp(arg, 'd') || strcmp(arg, 'deleta tudo')
+            
+            if contador_numeros > 0
+                
+                for a=1:contador_numeros
+                   clearvars lista_numeros(a) 
+                end
 
-            clearvars *_cz;
+                lista_numeros = cell(0);
+                contador_numeros = 0;
 
-            disp('todos os números criados foram deletados');
+                disp('todos os números criados foram deletados');
+                
+            else
+                
+                disp('erro: nada para deletar');
+            
+            end
 
-        % lista os números criados ou existentes
+        %% lista os números criados ou existentes
         elseif strcmp(arg, 'l') || strcmp(arg, 'lista')
 
-            disp('lista: ');
-            lista = who('*_cz');
-            disp(lista);
+            if contador_numeros > 0
+                disp(lista_numeros);
+            else
+                disp('erro: nenhum número criado');
+            end
 
         elseif strcmp(arg, 'v') || strcmp(arg, 'ver')
 
-            disp('lista: ');
-            lista = who('*_cz');
-            disp(lista);
+            if contador_numeros > 0
+                
+                disp('números cadastrados: ');
+                disp(lista_numeros);
 
-            nome = input('qual número? ', 's');
+                nome = input('qual número? ', 's');                              
 
-            eval(nome);
+                if strcmp(nome, 't') || strcmp(nome, 'todos')
+                    
+                    for c = 1:contador_numeros
+                        
+                        disp(' ');
+                        disp('------------------------------------------------------------');   
+                        fprintf('número: %s', char(lista_numeros(c)));
+                        
+                        eval(['valor= ' char(lista_numeros(c)) ';']);
+                        
+                        mostra_numero(valor);
+                        
+                    end  
+                    
+                    disp(' ');
+                    disp('------------------------------------------------------------');   
+                                       
+                else
+                    
+                    try
+                        eval(['valor =' nome ';']);
+                    catch
+                        disp('erro: número não existe');                       
+                        continue;
+                    end     
+                    
+                   mostra_numero(valor);
+                   
+                   disp(' ');
+                
+                end
+                
+            else
+                disp('erro: crie um número...');
+            end
 
         else
 
@@ -142,4 +208,35 @@ function z = criador
 
     end
 
+end
+
+function mostra_numero(valor)
+
+    global modo;
+
+    if modo.polar == 1
+        
+        if modo.graus == 1
+
+            num = z_cart_polar_graus(valor);
+
+            fprintf('\nraio          : %f', num.raio);
+            fprintf('\nângulo(graus) : %f', num.angulo);
+
+        elseif modo.graus == 0
+
+            num = z_cart_polar(valor);
+
+            fprintf('\nraio          : %f', num.raio);
+            fprintf('\nângulo(rad) : %f', num.angulo);
+
+        end
+
+    elseif modo.polar == 0
+
+        fprintf('\nreal : %f', real(valor));
+        fprintf('\nimag : %f', imag(valor));
+
+    end
+    
 end
