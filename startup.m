8594b99f3d0ff62badacd6fef99c61279b60718d
Felipe <felipeband18@gmail.com>
2013-01-10 17:08:02 -0300

    startup.m

---
+++ startup.m
@@ -0,0 +1,12 @@
+disp('aviso: iniciando, startup.m');
+
+disp('* mudando o diretorio');
+%cd D:\matematica\github ;
+cd D:\matematica\codigos_matlab ;
+
+disp('* mudando o formato dos numeros');
+format shorteng;
+
+disp('aviso: finalizado, startup.m');
+
+clear all;
