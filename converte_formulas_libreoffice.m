0b6a0141703f1f02f10f9225b02c2d923b313e67
Felipe <felipeband18@gmail.com>
2013-01-15 17:07:12 -0300

    ultimo backup antes da energia da bateria acabar! :(

--- converte_formulas_libreoffice.m
+++ converte_formulas_libreoffice.m
@@ -1,19 +1,5 @@
-% clc
-% clear all
-% syms a b
-% 
-% f = a/2 + b + a^2/2
-% 
-% pretty(f);
-% fs = char(f);
-% 
-% s = regexprep(fs, '/', 'over')
-
 function subterfugio = converte_formulas_libreoffice(formula)
 
 s = regexprep(formula, '/', ' over ');
-s = regexprep(s, '(', '{');
-s = regexprep(s, ')', '}');
-s = regexprep(s, '*', ' ');
 
 subterfugio = s;
