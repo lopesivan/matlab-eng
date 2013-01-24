a0d50b09236ab132b747a9d646b6e856c811ca94
Felipe <felipeband18@gmail.com>
2013-01-24 18:11:23 -0300

    pequeno exemplo

--- teoria_fasores/potencia_n_cargas.m
+++ teoria_fasores/potencia_n_cargas.m
@@ -1,7 +1,25 @@
 % Potência complexa de um sistema de n cargas em paralelos
-% entrada:
+% -- entrada:
 % potn = vetor com as potência das cargas
 % fpn = fator de potência das cargas
+%
+% -- exemplo:
+% Problema 12.31 Johnson
+% Duas carga em paralelo consomem respectivamente 210W com um fator de
+% potência de 0.6(adiantado) e 40W com um fator de potência de
+% 0.8(atrasado). Se a tensão através da associação em paralelo é Vg =
+% 50/15º V eficazes, calcule a corrente I entregue pela fonte.
+%
+% resolvendo:
+% do problema temos duas cargas, logo o vetor potn deve ser
+% p = [210, 40]
+% para o fator de potência deve ser levado em consideração a sua posição,
+% atrasada é positiva e adiantada é negativa
+% fp = [-0.6, 0.8]
+% colocando tudo no programa temos
+% potencia_n_cargas([210, 40], [-0.6, 0.8], 50)
+% nos levando a corrente entrege a fonte:
+% I = 5+j5
 function potencia = potencia_n_cargas(potn, fpn, vrms)
 %% processamento da entrada
 
