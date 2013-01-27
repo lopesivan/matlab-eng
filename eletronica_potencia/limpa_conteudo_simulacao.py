beaf69e5c96209a8614592eb0437ce47c6b5cddf
Felipe <felipeband18@gmail.com>
2013-01-27 17:47:20 -0300

    novas atualizações para o fourier novo backup agora é possivel deletar de forma automatizada os arquivos gerados pela simulacoes

--- eletronica_potencia/limpa_conteudo_simulacao.py
+++ eletronica_potencia/limpa_conteudo_simulacao.py
@@ -1,3 +1,6 @@
+# -*- coding: cp1252 -*-
+# Felipe Bandeira, 27/01/2013. Fortaleza-CE
+
 from glob import glob
 from os import walk, getcwd, path, remove
 
@@ -7,6 +10,9 @@ formatos_simulacao = [".smv"]
 print 'pasta atual:', getcwd()
 
 lista = []
+
+# Inicia a busca por um arquivo na pasta atual, finalizando quando
+# pecorre todos os arquivos e sub pastas
 for root, dirs, files in walk(dir_atual):
     for f in files:
         for a in formatos_simulacao:
@@ -16,18 +22,21 @@ for root, dirs, files in walk(dir_atual):
 
 if len(lista) > 0:
     print 'arquivo(s) encontrado(s):'
-    
     for q in lista:
         print q
-        try:
-            remove(q)
-        except:
-            print 'erro: nao foi possivel remover o arquivo,', q
 
-    print 'arquivo(s) removido(s)'
-    
+    apaga = raw_input('apagar o(s) arquivo(s) [S/n]?')
+    if apaga == 's' or apaga == '':
+        for q in lista:
+            try:
+                remove(q)
+                print q, ' --> deletado'
+            except:
+                print 'erro: nao foi possivel deletar, ', q
+    else:
+        print 'aviso: nenhum arquivo deletado'
 else:
-    print 'nada encontrado'
+    print 'aviso: nada encontrado'
 
 a = raw_input('[ENTER] para sair')
 
