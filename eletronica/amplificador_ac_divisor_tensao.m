55d7a9bfd6abef2b42c79da7ad20dca058a1be48
Felipe <felipeband18@gmail.com>
2013-02-14 16:12:00 -0300

    amplificador_ac_divisor_tensao.m

--- eletronica/amplificador_ac_divisor_tensao.m
+++ eletronica/amplificador_ac_divisor_tensao.m
@@ -1,16 +1,34 @@
-function subterfugio = amplificador_ac_divisor_tensao (R1, R2, RC, RE, VCC, RG, frequencia_base, tensao_max_sinal)
+% Amplificador clássico usando transistor bipolar, teoria Malvino.
+% - entrada:
+% r1, r2, re, rc, rf = em ohms
+% vcc = tensão de alimentação do circuito
+% bcc = ganho minimo do transistor
+% - exemplo:
+% amplificador_ac_divisor_tensao(10e3, 2.2e3, 1e3, 3.6e3, 10, 1e-3, 150, 1e3, 1.5e3)
+function dados = amplificador_ac_divisor_tensao(r1, r2, re, rc, vcc, vin, bcc, rf, rl)
 
-dados = npn_divisor_tensao(R1, R2, RC, RE, VCC);
+if nargin == 0
+    img = imread('fig_amplificador_ac_divisor_tensao.png');
+    imshow(img);
+    return;
+end
 
-% resitência vista pelo capacitor
-rth_cap = paralelos([R1, R2]) + RG;
+npn_dados = npn_divisor_tensao(r1, r2, re, rc, vcc, bcc);
 
-% cálculo do capacitor de acoplamento para 
-% freqüência de quina(frequencia base) usando a regra do 1:10
-C = 1/(2*pi*rth_cap*frequencia_base*(1/10));
+% todos os cálculos utilizam o modelo de Moll do transistor bipolar para
+% corrente alternada.
 
-% resitência do emissor
-re = dados.IE
+vbe_moll = 25e-3;
 
-subterfugio.C = C;
+re = vbe_moll/npn_dados.ie;             % resistência do emissor
+A = -rc/re;                             % ganho
+zent = 1/(1/r1 + 1/r2 + 1/(bcc*re));    % impedância de entrada
+vout_th = A*(vin*zent/(zent+rf));       % tensão théveni de saída
+vout = (vout_th*rl)/(rl+rc);            % tensão na carga rl
 
+%% saída
+
+dados.re = re;
+dados.ganho = A;
+dados.vout = vout;
+dados.zent = zent;
