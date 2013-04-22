d6ec1572fce40faf20e9c4e48e207a18569393fe
Felipe <felipeband18@gmail.com>
2013-04-22 19:20:38 -0300

    leves correções

--- teoria_fasores/cz.m
+++ teoria_fasores/cz.m
@@ -1,2 +1,9 @@
+% transformação indutância capacitiva para valor do capacitor
+% entrada:
+% z - impedância do capacitor
+% f - frequencia(Hertz)
 function c = cz(z, f)
-c = -1i/(2*pi*f*z);
+
+    c = -1i/(2*pi*f*z);
+    
+end
--- teoria_fasores/lz.m
+++ teoria_fasores/lz.m
@@ -1,2 +1,9 @@
+% Transformação impedância indutiva para valor do indutor
+% entrada
+% z - impedância do indutor
+% f - freqüência (Hertz)
 function l = lz(z, f)
-l = z/(2*pi*f*1i);
+
+    l = z/(2*pi*f*1i);
+    
+end
