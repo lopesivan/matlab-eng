4e00b875b3da21b9bd21ef4bb58d87f38aeb65f0
Felipe <felipeband18@gmail.com>
2013-06-25 17:08:06 -0300

    arquivo de configuracao implementado

--- aterramento/quadradoCheio.m
+++ aterramento/quadradoCheio.m
@@ -5,14 +5,15 @@
 function saida = quadradoCheio(m, n, esp, phoa, L, d)
 
 R = zeros(m, n);
-Rpropria = (phoa/(2*pi*L))*log((4*L)/(d*2.54e-2));
+%Rpropria = (phoa/(2*pi*L))*log((4*L)/(d*2.54e-2));
+Rpropria = (phoa/(2*pi*L))*log((4*L)/(d))
 
 for i=1:m
     for j=1:n
         for x=1:m
             for y=1:n
                 if (i==x)&&(j==y)
-                    R(i, j) = R(i, j)+Rpropria;
+                    R(i, j) = R(i, j)+Rpropria
                 else
                     e = sqrt((i-x)^2+(j-y)^2);
                     e = e*esp;
---
+++ aterramento/sistemasAterramento/py/configuracoes.py
@@ -0,0 +1,1 @@
+arquivoExcel = 'D:/matematica/codigos_matlab/aterramento/sistemasAterramento/py/tabelaExemplo2_12GeraldoKindermann.xlsx'
