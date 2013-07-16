88eaf104e316acf42b5ee046cb3c047e7612c534
Felipe Bandeira ufc <felipeband18@gmail.com>
2013-07-16 08:57:55 -0300

    documentacao, f

--- aterramento/sistemasAterramento/py/tmp/e31_calculationOfEarthResistanceForDeepDrivenRod.py
+++ aterramento/sistemasAterramento/py/tmp/e31_calculationOfEarthResistanceForDeepDrivenRod.py
@@ -1,4 +1,4 @@
-from scipy.integrate import quad
+from scipy.integrate import quad, romberg
 from scipy.special import jn
 from numpy import arange
 import matplotlib.pyplot as plt
@@ -17,7 +17,20 @@ def exemploMathWorks(grafico = False):
 		plt.grid(True)
 		plt.show()
 
-def Hs(hs): sum(hs)
+def Hs(h): 
+	sum(h)
+
+def HN1(h):
+	s = 0
+	for i in range(len(h)-1):
+		s+=h[i]
+	return s
+
+def pN(p):
+	return p[len(p)-1]
+
+def k(pm, pn1):
+	return (pn1-pn)/(pn1+pn)
 
 def eqTakahashiKawase():
 	l = 30
@@ -25,9 +38,27 @@ def eqTakahashiKawase():
 	p1 = 1
 	p2 = 100
 	a = .03
+	N = 2
+
+	s = 2
+
+	h = [1, 1]
+	p = [p1, p2]
+
+	ac = 0
+	for i in range(0, N):
+		ac += h[i]/p[i]
+
+	ac += 1/((k-HN1(h))/pN(p)) # fim da parte A
+
+	for s in range(1, N+1):
+		for i in range(0, s-1):
+			(1-k(p[i], p[i+1]))*romberg(quad(), H, Hs(h))
+
+
 	
-	pass
 
 if __name__ == '__main__':
 
 	exemploMathWorks()
+	eqTakahashiKawase()
