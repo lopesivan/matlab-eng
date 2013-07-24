40ca27aa222a9b8b42df5a6a2c9631b8e6fe0d85
Felipe Bandeira, armada110 <felipeband18@gmail.com>
2013-07-24 13:16:39 -0300

    incluindo o pyinstaller no diretorio do codigo fonte do projeto

---
+++ aterramento/sistemasAterramento/py/exe/pyinstaller/tests/basic/test_onefile_chdir_meipass.py
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+import os
+import sys
+
+os.chdir(sys._MEIPASS)
+
+import data6
+
+print(os.getcwd())
