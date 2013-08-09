3f464afe2beca62ec9788916ab3cf37a661f727c
Felipe, mint15 <felipeband18@gmail.com>
2013-08-09 15:45:49 -0300

    melhorando a interface

--- teoria_linhas/mainLinhas.py
+++ teoria_linhas/mainLinhas.py
@@ -4,6 +4,110 @@
 # felipeband18@gmail.com
 # graduando em Engenharia Elétrica
 
+from  __future__ import division
+import cargaModelagem
+import sys
+
+def dadosCargaMod():
+    try:
+        print u'potência aparente [MVA]           :',
+        potMVA = float(raw_input())
+        print u'fator de potência                 :',
+        fp = float(raw_input())
+        print u'tensão nominal da carga [kV]      :',
+        vn = float(raw_input())
+        print u'tensão de linha no barramento [kV]:',
+        vl = float(raw_input())
+    except:
+        print u'erro: inesperado'
+        return [-1, -1, -1, -1]
+
+    return [potMVA, fp, vn, vl]
+
+def entradaCargaMod():
+    print u'1- potência constante'
+    print u'2- impedância constante'
+    print u'3- carga mista'
+
+    try:
+        a = input('>')
+    except:
+        print u'erro: algo inesperado ocorreu na entrada do usuario'
+        return -2
+
+    if a == 1:
+        dados = dadosCargaMod()
+
+        if dados[0] == -1:
+            print u'erro: entrada inválida'
+            return -1
+
+        smod = cargaModelagem.potenciaConstante(dados[0], dados[1], dados[2], dados[3], debug = True)
+        corrente = cargaModelagem.correnteCarga(smod, dados[3], debug = True)
+        return 0
+
+    elif a == 2:
+        dados = dadosCargaMod()
+
+        if dados[0] == -1:
+            print u'erro: entrada inválida'
+            return -1
+
+        smod = cargaModelagem.impedanciaConstante(dados[0], dados[1], dados[2], dados[3], debug = True)
+        corrente = cargaModelagem.correnteCarga(smod, dados[3], debug = True)
+
+        return 0
+    elif a == 3:
+        dados = dadosCargaMod()
+
+        if dados[0] == -1:
+            print u'erro: entrada inválida'
+            return -1
+
+        try:
+            print u'PCTE %:',
+            pcte = float(raw_input())
+            print u'ZCTE %:',
+            zcte = float(raw_input())
+        except:
+            print u'erro: entrada inválida'
+            return -1
+
+        smod = cargaModelagem.cargaMista(dados[0], dados[1], dados[2], dados[3], pcte, zcte, debug = True)
+        corrente = cargaModelagem.correnteCarga(smod, dados[3], debug = True)
+
+        return 0
+    else:
+        print u'erro: opção não reconhecida'
+        return -1
+
+def ajuda():
+    print 'Comandos cadastrados'
+    print 'h - ajuda'
+
+def sair():
+    print u'finalizando...'
+    sys.exit(0)
+
+dicionarioComandos = {
+    'h' : ajuda,
+    's' : sair,
+    'm' : entradaCargaMod,
+}
+
+def nada():
+    print u'erro: comando não reconhecido'
+
+def comandos(cmd):
+    return dicionarioComandos.get(cmd, nada)()
 
 if __name__ == '__main__':
     print u'Transmissão de energia elétrica, linhas aéreas'
+
+    while True:
+        entrada = raw_input(':')
+        if entrada == '':
+            continue
+
+        comandos(entrada.lower())
+
