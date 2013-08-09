5c2f8ec941f06e2a6858ba46ff288689f8fc4e2d
Felipe, mint15 <felipeband18@gmail.com>
2013-08-09 14:59:32 -0300

    Mudando o nome cargaMista para cargaModelagem

---
+++ teoria_linhas/mainLinhas.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Felipe Bandeira da Silva
+# Fortaleza-CE, 29/06/2013
+# felipeband18@gmail.com
+# graduando em Engenharia Elétrica
+
+
+if __name__ == '__main__':
+    print u'Transmissão de energia elétrica, linhas aéreas'
