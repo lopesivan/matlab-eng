e6757afcf589b1d923f6a8f3b107930721ce2234
Felipe, mint15 <felipeband18@gmail.com>
2013-08-17 21:43:12 -0300

    Programa para a pratica 1 de servo finalizado

--- servo/pratica1.m
+++ servo/pratica1.m
@@ -2,7 +2,7 @@
 % Aluno:
 % - Felipe Bandeira da Silva, 1020942-X
 
-% 1 - Defina duas variáveis de números reais...
+disp('1 - Defina duas variáveis de números reais...')
 
 x = 1.1
 y = 2.2
@@ -16,7 +16,7 @@ r6 = sin(x) + cos(y)
 r7 = exp(x)*log(y)
 r8 = sqrt(x*y)
 
-% 2 - Defina duas variáveis de números complexos...
+disp('2 - Defina duas variáveis de números complexos...')
 
 cx = 1+2j
 cy = 2+3j
@@ -29,7 +29,7 @@ r13 = imag(cx/cy)
 r14 = x*conj(cy)
 r15 = atan(imag(cx)/real(cx))
 
-% 3 - Defina duas listas contendo cinco números reais...
+disp('3 - Defina duas listas contendo cinco números reais...')
 
 lx = [1 2 3 4 5]
 ly = [6;7;8;9;10]
@@ -41,18 +41,37 @@ r19 = lx.*ly'
 r20 = lx(5)/ly(2)
 r21 = lx+ly'
 
-% 4 - Defina uma matriz...
+disp('4 - Defina uma matriz...')
+
 M = [3 7 9; 1 5 5; 8 7 0]
 Mt = M'
 Ms = sum(M(1,:))
 Mi = inv(M)
 Md = det(M)
 
-% 5 - Crie um vetor de 1000 pontos...
-t = 0:0.01:10
+disp('5 - Crie um vetor de 1000 pontos...')
+t = 0:0.01:10;
 
-x1 = 5*exp(-t).*sin(2*t)+2*exp(-t).*cos(2*t)
-x2 = exp(-2*t)
+x1 = 5*exp(-t).*sin(2*t)+2*exp(-t).*cos(2*t);
+x2 = exp(-2*t);
 
 plot(t, x1, t, x2)
+
+disp('6 - Encontre o valor mínimo...')
+
+x = -20:0.01:20;
+f = x.^2-8*x+4;
+
+minimo = f(1);
+for I=2:length(f)
+    if f(I) < minimo
+        minimo = f(I);
+        posicao = I;
+    end
+end
+
+disp('Menor valor:')
+disp(minimo)
+disp('Posicao:')
+disp(posicao)
 
